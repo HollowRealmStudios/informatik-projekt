d6b37be66fc75cc5ded00d561b631fbdc3dc0a6e
NastyGamer <jones.m.987654321@gmail.com>
2019-05-07 18:13:25 +0200

    inital commit if libgdx files

---
+++ desktop/src/info/projekt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package info.projekt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.projekt.InfoProjekt;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InfoProjekt(), config);
+	}
+}
