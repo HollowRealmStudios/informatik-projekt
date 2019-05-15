37831be765caf95be1202837f2c7cff739c6b253
ConBuilder363 <conbuilder36@gmail.com>
2019-05-15 12:27:09 +0200

    .

--- core/src/info/projekt/christoph/TitleScreen.java
+++ core/src/info/projekt/christoph/TitleScreen.java
@@ -1,13 +1,59 @@
 package info.projekt.christoph;
 
+//https://stackoverflow.com/questions/25837013/switching-between-screens-libgdx
+//https://github.com/libgdx/libgdx/wiki/Extending-the-simple-game
+
+
+import com.badlogic.gdx.Screen;
+import info.projekt.InfoProjekt;
+
 /**
  * @author Christoph
  */
 
+public class TitleScreen implements Screen {
 
-public class TitleScreen {
 
 
+
+
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
 
 
