3724993e4c3a558fc73f694c0c844c31e715148b
NastyGamer <32023338+NastyGamer@users.noreply.github.com>
2019-05-24 22:21:36 +0200

    Small adjustment to global variables

--- desktop/src/info/projekt/desktop/DesktopLauncher.java
+++ desktop/src/info/projekt/desktop/DesktopLauncher.java
@@ -4,24 +4,25 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import info.projekt.InfoProjekt;
 import info.projekt.christoph.TitleScreen;
+import info.projekt.jonas.gui.RenderUtils;
 import info.projekt.jonas.storage.StorageHandler;
 
 import java.awt.*;
 import java.io.IOException;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) throws IOException {
-        TitleScreen TitleScreen = new TitleScreen();
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = Toolkit.getDefaultToolkit().getScreenSize().width;
-        config.height = Toolkit.getDefaultToolkit().getScreenSize().height;
-        config.fullscreen = true;
-        config.vSyncEnabled = false;
-        config.foregroundFPS = 60;
-        config.backgroundFPS = 60;
-        config.forceExit = true;
-        final LwjglApplication lwjglApplication = new LwjglApplication(new InfoProjekt(), config);
-        StorageHandler.registerArmors();
-        StorageHandler.registerWeapons();
-    }
+	public static void main(String[] arg) throws IOException {
+		TitleScreen TitleScreen = new TitleScreen();
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = RenderUtils.WIDTH;
+		config.height = RenderUtils.HEIGHT;
+		config.fullscreen = true;
+		config.vSyncEnabled = false;
+		config.foregroundFPS = 60;
+		config.backgroundFPS = 60;
+		config.forceExit = true;
+		final LwjglApplication lwjglApplication = new LwjglApplication(new InfoProjekt(), config);
+		StorageHandler.registerArmors();
+		StorageHandler.registerWeapons();
+	}
 }
