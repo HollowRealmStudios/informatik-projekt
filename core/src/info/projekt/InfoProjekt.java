c53958303f77d34db32c6de03dd7147ed90b2908
NastyGamer <32023338+NastyGamer@users.noreply.github.com>
2019-05-24 21:16:20 +0200

    Reimplemented move

--- core/src/info/projekt/InfoProjekt.java
+++ core/src/info/projekt/InfoProjekt.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import info.projekt.christoph.Screen;
+import info.projekt.jonas.gui.CameraManager;
 import info.projekt.jonas.gui.GameScreen;
 
 public class InfoProjekt extends Game {
@@ -14,6 +15,7 @@ public class InfoProjekt extends Game {
 	private static ShapeRenderer renderer;
 	private Screen screen;
 	private GameScreen gameScreen;
+	public static CameraManager manager;
 
 	@Override
 	public void create() {
@@ -21,12 +23,13 @@ public class InfoProjekt extends Game {
 		renderer = new ShapeRenderer();
 		screen = new Screen();
 		gameScreen = new GameScreen();
+		manager = new CameraManager();
 		setScreen(gameScreen);
 	}
 
 	@Override
 	public void render() {
-		Gdx.input.setInputProcessor((InputProcessor) getScreen());
+		batch.setProjectionMatrix(manager.getMatrix());
 		getScreen().render(0);
 	}
 }
--- core/src/info/projekt/jonas/gui/GameScreen.java
+++ core/src/info/projekt/jonas/gui/GameScreen.java
@@ -1,30 +1,43 @@
 package info.projekt.jonas.gui;
 
-import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.math.Vector2;
 import info.projekt.jonas.storage.GameStorage;
 
 import java.awt.*;
 
 import static info.projekt.InfoProjekt.batch;
+import static info.projekt.InfoProjekt.manager;
 
-public class GameScreen extends InputAdapter implements Screen {
+public class GameScreen implements Screen {
 
 	private GameStorage storage = new GameStorage();
 
-	public GameScreen() {
+	@Override
+	public void show() {
 		storage.debug();
 	}
 
 	@Override
-	public void show() {
-
-	}
-
-	@Override
 	public void render(float delta) {
+		keyDown();
 		RenderUtils.clearScreen(new Color(49, 144, 175));
 		RenderUtils.drawRooms(storage.getRooms(), batch);
+	}
+
+	private void keyDown() {
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			manager.translateRelative(new Vector2(0, Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) ? 20 : 10));
+		} else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+			manager.translateRelative(new Vector2(0, Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) ? -20 : -10));
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			manager.translateRelative(new Vector2(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) ? -20 : -10, 0));
+		} else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			manager.translateRelative(new Vector2(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) ? 20 : 10, 0));
+		}
 	}
 
 	@Override
@@ -50,11 +63,5 @@ public class GameScreen extends InputAdapter implements Screen {
 	@Override
 	public void dispose() {
 
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		System.out.println(keycode);
-		return false;
 	}
 }
