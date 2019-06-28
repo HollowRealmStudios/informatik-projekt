1394654603890152b45d2dec9b60f167796aa542
ConBuilder363 <conbuilder36@gmail.com>
2019-06-28 13:01:10 +0200

    added volume slider to settings, but you can´t change volume

--- core/src/info/projekt/christoph/Settings.java
+++ core/src/info/projekt/christoph/Settings.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -17,6 +18,7 @@ public class Settings {
 
     public Stage stage;
     private ImageButton close;
+    private Slider volumeSlider;
 
     public Settings() {
         stage = new Stage(new ScreenViewport());
@@ -24,7 +26,12 @@ public class Settings {
         close.setPosition(20f / 28f * WIDTH, 20f / 28f * HEIGHT);
         close.setSize(1f / 14f * WIDTH, 1f / 14f * HEIGHT);
         close.setVisible(false);
+        volumeSlider = new Slider(0f,100f,1f,true,new Slider.SliderStyle(new TextureRegionDrawable(new Texture("badlogic.jpg")),new TextureRegionDrawable(new Texture("Arrow.png"))));
+        volumeSlider.setPosition(12f / 28f * WIDTH, 7f / 28f * HEIGHT);
+        volumeSlider.setSize(1f / 14f * WIDTH, 7f / 14f * HEIGHT);
+        volumeSlider.setVisible(false);
         stage.addActor(close);
+        stage.addActor(volumeSlider);
 
     }
 
@@ -37,13 +44,13 @@ public class Settings {
                 hide();
             }
         });
-
+        volumeSlider.setVisible(true);
 
     }
 
     public void hide(){
         close.setVisible(false);
-
+        volumeSlider.setVisible(false);
     }
 
 
