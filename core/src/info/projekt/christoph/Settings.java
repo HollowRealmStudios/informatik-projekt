d476f1e6f474cc65129ec7dd4ff4fbcee3ced5b5
ConBuilder363 <conbuilder36@gmail.com>
2019-06-28 22:09:39 +0200

    sorted and commented Settings

--- core/src/info/projekt/christoph/Settings.java
+++ core/src/info/projekt/christoph/Settings.java
@@ -27,63 +27,77 @@ public class Settings {
     private float[] values;
     public float volume;
     private Music music;
-    private Music andi;
+
 
     public Settings() {
+        //Creating a new stage
+        stage = new Stage(new ScreenViewport());
+
+        //define the volume of the music, start the music and loop it, define the snapvalues of the volumeslider, define the song
         volume = 100f;
-        music = Gdx.audio.newMusic(Gdx.files.internal("Nein jetzt hältst du die Schnauze! (Remix).mp3"));
-        andi = Gdx.audio.newMusic(Gdx.files.internal("Nein jetzt hältst du die Schnauze! (Remix).mp3"));
-        for(int i = 0; i < 20; i++) {
-        	Gdx.audio.newMusic(Gdx.files.internal("Nein jetzt hältst du die Schnauze! (Remix).mp3")).play();
-	        try {
-		        Thread.sleep(200);
-	        } catch (InterruptedException e) {
-		        e.printStackTrace();
-	        }
-        }
         music.play();
         music.setLooping(true);
-        values = new float[]{0,10,20,30,40,50,60,70,80,90,100};
-        stage = new Stage(new ScreenViewport());
+        values = new float[]{0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        music = Gdx.audio.newMusic(Gdx.files.internal("Nein jetzt hältst du die Schnauze! (Remix).mp3"));
+
+        //creating a close button, set its position and size, set it invisible
         close = new ImageButton(new TextureRegionDrawable(new Texture("badlogic.jpg")));
         close.setPosition(20f / 28f * WIDTH, 20f / 28f * HEIGHT);
         close.setSize(1f / 14f * WIDTH, 1f / 14f * HEIGHT);
         close.setVisible(false);
-        volumeSlider = new Slider(0f,100f,1f,false,new Slider.SliderStyle(new TextureRegionDrawable(new Texture("Slider.png")),new TextureRegionDrawable(new Texture("Arrow.png"))));
+
+        //creating a volumeslider ( define its minimum and maximum, the step size and 1: slider; 2: knob pictures ),set its position, set its size, set its snapvalues, set it invisible, set its value to the current volume level
+        volumeSlider = new Slider(0f, 100f, 1f, false, new Slider.SliderStyle(new TextureRegionDrawable(new Texture("Slider.png")), new TextureRegionDrawable(new Texture("Arrow.png"))));
         volumeSlider.setPosition(7f / 28f * WIDTH, 14f / 28f * HEIGHT);
         volumeSlider.setSize(7f / 14f * WIDTH, 1f / 28f * HEIGHT);
         volumeSlider.setSnapToValues(values, 2.5f);
         volumeSlider.setVisible(false);
         volumeSlider.setValue(volume);
+
+        //add all actors to the stage
         stage.addActor(close);
         stage.addActor(volumeSlider);
 
     }
 
 
-    public void show(){
+    public void show() {
+
+        //setting all actors visible and add their listener
         close.setVisible(true);
         close.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                andi.play();
-                andi.setLooping(true);
+                hide();
             }
         });
+
+
         volumeSlider.setVisible(true);
         close.setVisible(true);
         volumeSlider.addListener(new ChangeListener() {
             @Override
-            public void changed (ChangeEvent event, Actor actor){
+            public void changed(ChangeEvent event, Actor actor) {
                 volume = volumeSlider.getValue();
                 music.setVolume(volume / 100);
+            }
+        });
 
-			}
-		});
-	}
+    }
 
-	public void hide() {
-		close.setVisible(false);
-		volumeSlider.setVisible(false);
-	}
+    public void hide() {
+
+        //set all actors invisible
+        close.setVisible(false);
+        volumeSlider.setVisible(false);
+    }
 }
+
+   /* for(int i = 0; i < 20; i++) {
+        	Gdx.audio.newMusic(Gdx.files.internal("Nein jetzt hältst du die Schnauze! (Remix).mp3")).play();
+	        try {
+		        Thread.sleep(200);
+	        } catch (InterruptedException e) {
+		        e.printStackTrace();
+	        }
+        }*/
