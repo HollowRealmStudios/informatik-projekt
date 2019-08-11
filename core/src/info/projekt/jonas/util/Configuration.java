961e83a7f6de3932918be883cc5abf350e66b3e8
Jonas <jones.m.987654321@gmail.com>
2019-08-11 23:13:31 +0200

    Added meds, added zipped test version

--- core/src/info/projekt/jonas/util/Configuration.java
+++ core/src/info/projekt/jonas/util/Configuration.java
@@ -4,15 +4,15 @@ public class Configuration {
 
 	public static final class Debug {
 		public static final boolean FPS_COUNTER = false;
-		public static final boolean GUI_DEBUG = true;
+		public static final boolean GUI_DEBUG = false;
 		public static final boolean REGISTRY_DEBUG = false;
 	}
 
 	public static final class Resources {
-		public static final float MINUTES_PER_DWELLER = 0.2f;
+		public static final float MINUTES_PER_DWELLER = 2f;
 		public static final float RESOURCE_MULTIPLIER = 1f;
 		public static final float MINUTES_PER_CHILDREN = 1f;
-		public static final float MONEY_MULTIPLIER = 100f;
+		public static final float MONEY_MULTIPLIER = 2f;
 	}
 
 	public static final String FILE_NAME = "Storage.dat";
